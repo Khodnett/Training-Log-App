1872c59db2355d10ba8e8d6b3acc36aa15fa89f2
Kaleb Hodnett <kalebhodnett@gmail.com>
2022-12-26 23:05:25 -0600

    Adding gitignore

--- fit_project/fit_project/settings.py
+++ fit_project/fit_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import sys
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'fit_tracker/templates/fit_tracker')
@@ -21,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'fit_tracker/templates/fit_tracker')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#8m*m5v_i@9we+%-h#7^m42^*ioah$09b%zk@y=mmp2-)bxfet'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
